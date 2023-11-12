bf7da85f6ea1df4cef43da841b5ca4eb23715d02
Muhammad Ikhsan Fauzi <137757092+IkhsanFreya@users.noreply.github.com>
2023-11-12 14:54:25 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,30 @@
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  var typed = new Typed(".katakata", {
+    strings: [
+      "Cuma mau bilang",
+      "Selamat Sore SILMIE",
+      "Jangan lupa makann yawww",
+      "Gaboleh diet diet yahhhh",
+    ],
+    typeSpeed: 70,
+  });
+}
+function clicks() {
+  $("#div1").hide();
+  $("#div2").show();
+  $("#div2").addClass("fadeinv2");
+
+  var typed = new Typed(".katakata", {
+    strings: [
+      "Cuma mau bilang",
+      "Selamat Sore SILMIE",
+      "Jangan lupa makan yawww",
+      "Gaboleh diet diet yahhh kmu",
+    ],
+    typeSpeed: 70,
+  });
+}
